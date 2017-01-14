740430c03082921c83404f2d091b5391a2d87c65
André Saage <andre.saage@gmail.com>
2017-01-14 13:32:26 +0100

    Update bird.js

--- examples/bird.js
+++ examples/bird.js
@@ -1,23 +1,26 @@
 console.log("bird");
 const count = 12;
 let a = [];
-let cam;
+//let cam;
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL).parent('container');
   for (let i=0; i<count; i++){
     a.push(createVector(random(width),random(height),0));
   }
-  cam = createVector(200, 200, 200);
-  //translate(-width/2,-height/2,0);
+  
+  //cam = createVector(200, 200, 200);
 }
 
 function draw() {
   background(228,0,43);
-  camera(0, 0, sin(frameCount * 0.01) * 100);
-  stroke(2);
+  orbitControl();
+  
   //beginShape();
-  for (let i=0; i<count-1; i++){
-    line(a[i].x, a[i].y, a[i].z, a[i+1].x, a[i+1].y, a[i+1].z);
+  for (let i=0; i<count; i++){
+    push();
+    translate(a[i].x, a[i].y, a[i].z);
+    sphere(20);
+    pop();
   }
   //endShape(CLOSE);
 }
