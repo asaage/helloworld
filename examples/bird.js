c9bc5d5de56c61395ea54ae2c3f1921f0851aa9e
André Saage <andre.saage@gmail.com>
2017-01-14 11:06:01 +0100

    Update bird.js

--- examples/bird.js
+++ examples/bird.js
@@ -1,1 +1,19 @@
 console.log("bird");
+const dots = 12;
+let a = [];
+function setup() {
+  createCanvas(720, 400);
+  for (let i=0; i<dots; i++){
+    a[i]=random(width);
+  }
+}
+
+function draw() {
+  background(55);
+  stroke(2);
+  beginShape();
+  for (let i=0; i<dots; i++){
+    vertex(a[i], random(heigth));
+  }
+  endShape(CLOSE);
+}
